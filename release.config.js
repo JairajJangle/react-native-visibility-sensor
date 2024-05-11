78fa8ae3daf5b0b4bd72df0f553b4914b16b8a67
Jairaj Jangle <jairaj.jangle@gmail.com>
2024-05-11 09:21:14 +0530

    fix(semantic_release): release config fix for assets

--- release.config.js
+++ release.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    '@semantic-release/commit-analyzer', // Analyzes commits for version bumping
+    '@semantic-release/release-notes-generator', // Generates release notes
     '@semantic-release/changelog', // Generates the changelog
+    '@semantic-release/git', // Commits changed files including the changelog
     [
-      '@semantic-release/github', // Manages the GitHub release
+      '@semantic-release/github', // Manages the GitHub release and uploads assets
       {
         assets: [{ path: 'CHANGELOG.md', label: 'Changelog' }],
       },
     ],
-    '@semantic-release/git', // Commits changed files including the changelog
     '@semantic-release/npm', // Handles npm publishing
   ],
 };
